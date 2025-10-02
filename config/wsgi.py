695c13533054d3104b8f0bc2e6bc9702755752d7
Eugenio829 <jimenezeugenio11@gmail.com>
2025-10-02 12:18:15 -0400

    confi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Añade la ruta de tu proyecto al sistema.
+# Asegúrate de que la ruta '/home/Eugeniojiemenz17/cigar_rings' apunta
+# al directorio que contiene tu archivo manage.py.
+path = '/home/Eugeniojiemenz17/cigar_rings'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Apunta a tu archivo de settings.
+# La carpeta que contiene settings.py se llama 'config', por eso es 'config.settings'.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+
+# Carga la aplicación WSGI de Django.
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
